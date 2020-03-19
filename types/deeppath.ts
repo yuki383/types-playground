f82273b66b7e1014d28dff375288ecbbeeaa100b
yuu383 <yuukikokubo.pg@gmail.com>
2020-03-19 22:16:22 +0900

    implement DeepPath

--- types/deeppath.ts
+++ types/deeppath.ts
@@ -1,3 +1,4 @@
+// Quote: https://qiita.com/hrsh7th@github/items/84e8968c3601009cdcf2
 type Head<T extends any[]> = ((...args: T) => any) extends (
   head: infer U,
   ...args: any[]
@@ -11,3 +12,24 @@ type Tail<T extends any[]> = ((...args: T) => any) extends (
 ) => any
   ? U
   : never;
+
+type DeepPath<T, K extends any[]> = Head<K> extends keyof T
+  ? {
+      0: T[Head<K>];
+      1: DeepPath<T[Head<K>], Tail<K>>;
+    }[Tail<K> extends [] ? 0 : 1]
+  : never;
+
+const obj = {
+  key1: {
+    key2: {
+      key3: true,
+    }
+  }
+} as const
+
+let test: DeepPath<typeof obj, [
+  "key1",
+  "key2",
+  "key3"
+]> = true
